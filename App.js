3debd94c77179a78c6ebf5738edc0201adea5403
felipekako <felipekakosampaio@gmail.com>
2022-08-11 15:05:55 -0300

    subindo o cinema

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View,Button } from 'react-native';
+import {useState} from 'react';
 export default function App() {
+  const [nome,setnome] = useState('cebola');
+  const [sessao, setSessao] = useState('miions');
+  const [exibindo, setExibindo] = useState('true');
+
+  const clicado = () =>{
+    setSessao('thor');
+    setnome('batata');
+    setExibindo('false');
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}> { nome}</Text>
+      <Text style={styles.titulo}> { sessao}</Text>
+      <Text style={styles.titulo}> { exibindo ? "esta em sessão" : "acabou:("}</Text>
       <StatusBar style="auto" />
+      <Button title ='trocar sesão' onPress={clicado} />
     </View>
   );
 }
@@ -13,8 +25,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#64732F',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    color:"#D99036",
+    fontSize:30,
+  }
 });
